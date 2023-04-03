ce8d84048fac344cb8a377a58f17b28f8d47f38e
ValeryOvares <123129685+ValeryOvares@users.noreply.github.com>
2023-04-03 16:51:31 -0600

    Add files via upload

---
+++ src/main/java/com/proyectolbd/ProyectolbdApplication.java
@@ -0,0 +1,13 @@
+package com.proyectolbd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectolbdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectolbdApplication.class, args);
+	}
+
+}
